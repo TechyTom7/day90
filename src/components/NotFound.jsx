756d6b39b9936dc64ae14985ff11753ba2de1727
TechyTom7 <purpletom007@gmail.com>
2024-08-13 15:07:18 +0200

    Updated background color

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,7 +2,10 @@ import consts from "../consts";
 import "../styles/NotFound.css"
 
 export default function NotFound() {
-    <div id='not-found-container'>
-        <h1>404 Page not found</h1>
-    </div>
+
+    return (
+        <div id='not-found-container'>
+            <h1>404 Page not found</h1>
+        </div>
+    )
 }
