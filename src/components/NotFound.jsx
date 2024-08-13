9a3d9d4333d0f54650a4764ebcda0b7ff6f0d4bb
TechyTom7 <purpletom007@gmail.com>
2024-08-13 15:21:12 +0200

    Updated background color

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,11 +1,13 @@
 import consts from "../consts";
 import "../styles/NotFound.css"
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
 
     return (
         <div id='not-found-container'>
             <h1>404 Page not found</h1>
+            <p>Redirect to <Link to="/">Home page</Link> </p>
         </div>
     )
 }
