02dc9d4016a7ca4e7251efe691279791b9219115
TechyTom7 <purpletom007@gmail.com>
2024-08-27 13:49:23 +0200

    Added name to profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -32,6 +32,10 @@ export default function Profile(props) {
                 <h1>Profile</h1>
                 <table>
                     <tr>
+                        <td>Name</td>
+                        <td>{user.name}</td>
+                    </tr>
+                    <tr>
                         <td>Email</td>
                         <td>{user.email}</td>
                     </tr>
